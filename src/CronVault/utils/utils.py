1b4b0d3f8c9f6b327a678fc4b726bf60d0c98a68
Stefanos Mytilinaios <2906344M@student.gla.ac.uk>
2026-07-26 13:38:34 +0300

    add first backup implementation

--- src/CronVault/utils/utils.py
+++ src/CronVault/utils/utils.py
@@ -5,8 +5,11 @@ import os
 import json
 import re
 import logging
+import pathvalidate
+import send2trash
 from datetime import datetime
 import pytimeparse
+import shutil
 from pathlib import Path
 from typing import Any
 from colorama import Fore, Style
@@ -15,6 +18,8 @@ from .json_schema import SCHEMA
 
 CONFIG_LOCATION: str = "~/.config/CronVault/"
 MAX_NAME_ATTEMPTS: int = 101
+MAX_DELETE_OLD_BACKUP_ATTEMPTS: int = 10
+CRONVAULT_MARKER_FILENAME: str = ".cronvault_marker.json"
 
 
 def parse_name(name: str) -> str:
@@ -308,12 +313,179 @@ def delete_backup(
     file_path = file_path / f"{name}.json"
     try:
         if file_path.exists():
-            file_path.unlink()
+            send2trash.send2trash(file_path)
             logging.info("Successfully deleted file")
         else:
             logging.error(f"No such config found: {name}. Exiting")
     except OSError:
         logging.error(f"Encountered IOError while trying to delete config file {name}")
+        raise
+
+
+def get_directory_size(path: Path) -> int:
+    return sum(
+        f.stat(follow_symlinks=False).st_size for f in path.rglob("*") if f.is_file()
+    )
+
+
+def check_file_for_backup(
+    name: str,
+    skip_checks: bool = False,
+    file_path: Path = Path(CONFIG_LOCATION).expanduser(),
+) -> None:
+    logging.info(f'Attempting to backup "{name}"')
+    file_path = file_path / f"{name}.json"
+    try:
+        if file_path.exists():
+            config = json.loads(file_path.read_text())
+            validate(instance=config, schema=SCHEMA)
+
+            previous_backup = config["last_known_backup"]
+            if previous_backup is None:
+                previous_backup = datetime.fromisoformat("1970-01-01")
+            else:
+                previous_backup = datetime.fromisoformat(previous_backup)
+
+            time_period_elapsed = (previous_backup - datetime.now()).seconds >= config[
+                "time_period"
+            ]
+
+            is_active: bool = config["status"] == "active"
+
+            if skip_checks or (time_period_elapsed and is_active):
+                perform_backup(config)
+
+            config["last_known_backup"] = datetime.now().isoformat()
+            config["total_backup_count"] += 1
+            file_path.write_text(json.dumps(config))
+
+            logging.info(f"Successfully backed up {name}, and wrote changes to config")
+        else:
+            logging.error(f"No such config found: {name}. Exiting")
+    except (json.JSONDecodeError, ValidationError) as e:
+        logging.error(
+            f'Config file "{name}" is malformed or corrupted. View details with --verbose'
+        )
+        logging.debug(e)
+    except OSError:
+        logging.error(f"Encountered OSError while trying to access config file {name}")
+        raise
+
+
+def find_oldest_backup(file_path: Path) -> Path | None:
+    """
+    Finds oldest CronVault backup in `file_path`. Skips other directories
+    """
+    result: Path | None = None
+    logging.info(f"Finding oldest backup in {file_path}")
+    try:
+        if len(list(file_path.iterdir())) == 0:
+            return None
+
+        creation_dates = {
+            subdirectory.stat().st_birthtime: subdirectory
+            for subdirectory in file_path.iterdir()
+        }
+
+        while (not result) and creation_dates:
+            oldest_backup_time = min(creation_dates)
+            candidate = creation_dates.get(oldest_backup_time, None)
+            if not candidate:
+                break
+            logging.info(f"Oldest directory in {file_path} is {candidate}")
+            backup_marker = candidate / CRONVAULT_MARKER_FILENAME
+            if not backup_marker.exists():
+                logging.info(f"{candidate} is not a CronVault backup, skipping...")
+                creation_dates.pop(oldest_backup_time)
+                continue
+            result = candidate
+
+    except OSError:
+        logging.error(f"Error while trying to access backups in {file_path}. Aborting")
+        raise
+
+    return result
+
+
+def generate_cronvault_marker(folder_path: Path, backup_folder_path: Path) -> str:
+    marker = {
+        "original_folder": folder_path,
+        "backup_datetime": datetime.now().isoformat(),
+        "backup_folder_path": backup_folder_path,
+    }
+    return json.dumps(marker)
+
+
+def get_device_free_space(backup_folder_path: Path) -> int:
+    _, _, free = shutil.disk_usage(backup_folder_path)
+    return free
+
+
+def perform_backup(config: dict[str, Any], path_override=None) -> None:
+    """config must be valid and already checked"""
+    #  in the future, add notification support
+    logging.info(f"Performing backup for config {config['name']}")
+    folder_path = Path(config["path"])
+    backup_folder_path = (
+        Path(path_override) if path_override else Path(config["destination"])
+    )
+    max_storage_limit = config["max_backup_size"]
+
+    try:
+        for _ in range(MAX_DELETE_OLD_BACKUP_ATTEMPTS):
+            exceeds_storage_limit: bool = (
+                get_directory_size(folder_path) + get_directory_size(backup_folder_path)
+            ) > max_storage_limit or get_directory_size(
+                folder_path
+            ) > get_device_free_space(backup_folder_path)
+            if not exceeds_storage_limit:
+                logging.info("Enough space to perform backup. Proceeding.")
+                break
+
+            oldest_backup = find_oldest_backup(backup_folder_path)
+            if oldest_backup is None:
+                logging.error(
+                    f"Not enough space in {backup_folder_path} to back up {folder_path}. Exiting"
+                )
+                return
+
+            #  probably smarter idea to send backup to trash than immediately delete
+            logging.info(
+                f"Not enough storage space to backup. Sending oldest backup: {oldest_backup} to trash"
+            )
+            send2trash.send2trash(oldest_backup)
+            #  maybe good idea to ask user before deleting
+            #  but that eliminates automation
+
+        else:
+            logging.error(
+                "Reached maximum number of older backup deletion attempts. User intervention requried."
+            )
+            return
+
+        backup_name = datetime.strftime(datetime.now(), config["name_format"])
+        destination = backup_folder_path / backup_name
+
+        if not pathvalidate.is_valid_filepath(destination, platform="auto"):
+            logging.error(
+                f"Error: filepath {destination} is not valid. Backup cannot be performed. Exiting"
+            )
+            return
+
+        destination.mkdir()
+
+        #  `copy_into` was introduced in Python 3.14. This line should work, but pyright isn't picking it up for some reason...
+        logging.info(f"Copying contents of {folder_path} to {destination}...")
+        folder_path.copy_into(destination, preserve_metadata=True)  # pyright: ignore
+        logging.info("Copying complete")
+        cronvault_marker = destination / CRONVAULT_MARKER_FILENAME
+        cronvault_marker.write_text(
+            generate_cronvault_marker(folder_path, backup_folder_path)
+        )
+        logging.info(f"Wrote CronVault marker to {cronvault_marker}")
+    except OSError:
+        logging.error("Encountered OSError while performing backup. Aborting...")
+        #  maybe add cleanup function?
         raise
 
 
